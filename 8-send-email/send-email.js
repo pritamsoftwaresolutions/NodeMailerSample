6d3b2f40370dfc193d13bec2900731d52c1aa168
Srinath <srinathvangari@gmail.com>
2021-06-06 12:30:15 +0530

    First Commit Srinath

--- 8-send-email/send-email.js
+++ 8-send-email/send-email.js
@@ -1,18 +1,34 @@
 var nodemailer = require('nodemailer');
 
+// var transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: 'srinathvangari@gmail.com',
+//     pass: '**********'
+//   }
+// });
+
 var transporter = nodemailer.createTransport({
-  service: '',
+  service: 'gmail',
+  port:465,
+  secure: true, // true for 465, false for other ports
+  logger: true,
+  debug: true,
+  secureConnection: false,
   auth: {
-    user: '',
-    pass: ''
+      user: 'srinathvangari@gmail.com', // generated ethereal user
+      pass: '******' // generated ethereal password
+  },
+  tls:{
+      rejectUnAuthorized:true
   }
-});
+})
 
 var mailOptions = {
-  from: '',
-  to: '',
-  subject: '',
-  text: ``
+  from: 'srinathvangari@gmail.com',
+  to: 'srinathvangari@yahoo.com',
+  subject: 'Hello NOEMAIL TEST',
+  text: `HI Hello Thank node mail`
 };
 
 transporter.sendMail(mailOptions, function(error, info){
